422e86dcbaefb77aee1836b02fc5391258c749b8
Hana <jlucy0411@gmail.com>
2021-12-25 19:55:15 -0600

    I think I need to restart all the classes again. Naming wise its awful it doesn't even make sense. But its practice after all lol

--- src/Arrays.java
+++
@@ -1,3 +0,0 @@
-public class Arrays {
-    // Big -0 Notion lower, avarage time, Upper Bound
-}
---
+++ src/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println(" main");
+        // allocating memory for 5 integers.
+    }
+
+}
